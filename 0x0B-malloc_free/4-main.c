3d57ce7c7290ef870d52cb594e28f81e4f9dc58b
Ridawn887 <akoredeayobami973@gmail.com>
2023-04-13 02:59:03 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-main.c
+++ 0x0B-malloc_free/4-main.c
@@ -12,23 +12,24 @@
  */
 void print_grid(int **grid, int width, int height)
 {
-    int w;
-    int h;
+	int w;
+	int h;
 
-    h = 0;
-    while (h < height)
-    {
-        w = 0;
-        while (w < width)
-        {
-            printf("%d ", grid[h][w]);
-            w++;
-        }
-        printf("\n");
-        h++;
-    }   
+	h = 0;
+	while (h < height)
+	{
+		w = 0;
+		while (w < width)
+		{
+			printf("%d ", grid[h][w]);
+			w++;
+			printf("\n");
+			w++;
+		}
+		printf("\n");
+		h++;
+	}
 }
-
 /**
  * main - check the code for ALX School students.
  *
@@ -36,18 +37,18 @@ void print_grid(int **grid, int width, int height)
  */
 int main(void)
 {
-    int **grid;
+	int **grid;
 
-    grid = alloc_grid(6, 4);
-    if (grid == NULL)
-    {
-        return (1);
-    }
-    print_grid(grid, 6, 4);
-    printf("\n");
-    grid[0][3] = 98;
-    grid[3][4] = 402;
-    print_grid(grid, 6, 4);
-    free_grid(grid, 4);
-    return (0);
+	grid = alloc_grid(6, 4);
+	if (grid == NULL)
+	{
+		return (1);
+	}
+	print_grid(grid, 6, 4);
+	printf("\n");
+	grid[0][3] = 98;
+	grid[3][4] = 402;
+	print_grid(grid, 6, 4);
+	free_grid(grid, 4);
+	return (0);
 }
