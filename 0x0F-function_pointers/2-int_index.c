4c69a77b840ab878cab0858a786c411dd4f9a38f
Ridawn887 <akoredeayobami973@gmail.com>
2023-04-19 12:20:57 -0700

    do add things

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-include "function_pointers.h"
+#include "function_pointers.h"
 /**
  * int_index - return index place if comparison = true, else -1
  * @array: array
@@ -8,15 +8,21 @@ include "function_pointers.h"
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i;
+	int i = 0;
 
-	if (array == NULL || size <= 0 || cmp == NULL)
-		return (-1);
-
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		if (cmp(array[i]))
-			return (i);
+		if (array != NULL && cmp != NULL)
+		{
+			while (i < size)
+			{
+				if (cmp(array[i]))
+					return (i);
+
+				i++;
+			}
+		}
 	}
+
 	return (-1);
 }
